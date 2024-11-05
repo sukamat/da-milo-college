90d7c602f42fcd2c7755bd7ca5da2d361f45ecd5
sukamat <sukamat@adobe.com>
2024-11-04 17:04:47 -0800

    default path updated

--- tools/floodbox/graybox/graybox.js
+++ tools/floodbox/graybox/graybox.js
@@ -224,7 +224,7 @@ export default class MiloFloodgate extends LitElement {
       <h1>Graybox</h1>
       <form @submit=${this.handleSubmit}>
         <div class="input-row">          
-          <input class="path" name="path" value="/sukamat/da-milo-college-graybox/temp" @input=${this.validateInput} />
+          <input class="path" name="path" value="/sukamat/da-milo-college-graybox/bulk-2" @input=${this.validateInput} />
           <button class="accent" .disabled=${!this._canPromote}>Promote</button>
           <button class="primary" @click=${this.handleCancel}>Cancel</button> 
         </div>       
